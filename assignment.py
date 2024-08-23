5db163e5c3d8805e8433b00ff9d3e4eb9a29778a
Machine <minralquartz@gmail.com>
2024-08-23 18:07:25 +0300

    first commit

---
+++ assignment.py
@@ -0,0 +1,29 @@
+MyName = "Marita Machine"
+MyAge = "15"
+MyFavoriteColor = "White"
+TheCityILiveIn = "Sabaki"
+ #print functions
+print(MyName)
+print(MyAge)
+print(MyFavoriteColor)
+print(TheCityILiveIn)
+#f-string
+print(f"My name is {MyName}, I am {MyAge} years old. My favourite colour is {MyFavoriteColor} and I live in {TheCityILiveIn}.")
+#part 2
+firstnumber = 6
+secondnumber = 7
+print(firstnumber+secondnumber)
+print(firstnumber-secondnumber)
+print(firstnumber*secondnumber)
+print(firstnumber/secondnumber)
+#updating variables
+currentage = 15
+futureage = (currentage+5)
+print(f"In five years I will be {futureage}")
+#part 3
+word = "doesn't"
+quote =   (f'"Be thankful of past you, for he made choices present you {word} want to."')
+print(f"The quote, {quote}, was said by {MyName}.")
+#personalized greeting
+friendname = "Jamie Kanango"
+print(f"Hello {friendname}!Its {MyName}.")
